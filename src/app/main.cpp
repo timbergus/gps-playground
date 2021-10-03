541713d309a0f58c3941d962267aba177c90ae9e
Gustavo Muñoz <timbergus@gmail.com>
2021-10-03 18:46:45 +0200

    Read only one type of message.

--- src/app/main.cpp
+++ src/app/main.cpp
@@ -4,10 +4,17 @@
 
 int main()
 {
+  std::cout << "Hello World!" << std::endl;
+
   GPS gps;
 
   gps.get_data_stream("./src/data/samples.txt", [&gps](std::string sample)
-                      { gps.parse_sample(sample); });
+                      {
+                        if (sample.substr(1, 5) == "GNRMC")
+                        {
+                          gps.parse_sample(sample);
+                        }
+                      });
 
   return EXIT_SUCCESS;
 }
