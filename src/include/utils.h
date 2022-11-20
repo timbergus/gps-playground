b2520ae50f349bdadfec55ed2ce8ee070d4d3ae6
Gustavo Muñoz <timbergus@gmail.com>
2022-11-20 03:07:49 +0100

    Add is valid sample validation and clear screen.

--- src/include/utils.h
+++ src/include/utils.h
@@ -18,16 +18,18 @@ public:
   ~Utils();
 
   static std::vector<std::string> split(std::string, std::string);
-  static std::vector<std::string> clean_split(std::string, std::string);
   static std::string stringify_vector(std::vector<std::string>);
   static std::string stringify_map(std::map<std::string, std::string>);
 
+  static bool is_valid_sample(std::string);
   static std::string get_type(std::string);
   static std::string get_time(std::string);
   static std::string get_date(std::string, std::string = "sp");
   static double get_latitude(std::string);
   static double get_longitude(std::string, std::string);
   static double get_speed(std::string, std::string = "ms");
+
+  static void clear_screen();
 };
 
 Utils::Utils()
@@ -38,51 +40,21 @@ Utils::~Utils()
 {
 }
 
-std::vector<std::string> Utils::clean_split(std::string sample, std::string delimeter)
-{
-  std::vector<std::string> result;
-
-  char *token = strtok(&sample[0], delimeter.c_str());
-
-  while (token)
-  {
-    result.push_back(token);
-    token = strtok(NULL, delimeter.c_str());
-  }
-
-  return result;
-}
-
 std::vector<std::string> Utils::split(std::string sample, std::string delimeter)
 {
   // This is a strings vector where we are going to store the splitted string.
   std::vector<std::string> result;
 
-  // And this the initial possition of the string.
-  int initial_position = 0;
+  // And this the initial position of the string.
+  std::string::size_type beg = 0;
 
-  // This is the first search position.
-  std::size_t found_at = sample.find(delimeter);
-
-  /**
-   * And here the substring is stored into the result vector. The substring is
-   * the string from the beginning to the first delimeter. It gets data from
-   * the initial position to the length we pass to the function. So the length
-   * will be (initial_position - found_at).
-   */
-
-  result.push_back(sample.substr(initial_position, found_at));
-
-  // We always need to move one fordware because we need to pass the previous
-  // search position.
-  initial_position = found_at + 1;
-
-  while (found_at != std::string::npos)
+  for (auto end = 0; size_t(end = sample.find(delimeter, end)) != std::string::npos; ++end)
   {
-    found_at = sample.find(",", initial_position);
-    result.push_back(sample.substr(initial_position, found_at - initial_position));
-    initial_position = found_at + 1;
+    result.push_back(sample.substr(beg, end - beg));
+    beg = end + 1;
   }
+
+  result.push_back(sample.substr(beg));
 
   return result;
 }
@@ -117,6 +89,28 @@ std::string Utils::stringify_map(std::map<std::string, std::string> tokens)
   result += " }";
 
   return result;
+}
+
+// https://www.tigoe.com/pcomp/code/Processing/127
+
+bool Utils::is_valid_sample(std::string sample)
+{
+  std::vector<std::string> fragments{Utils::split(sample, "*")};
+
+  std::string data = fragments[0].substr(1);
+
+  int check = 0;
+
+  for (int i = 0; i < (int)data.size(); i++)
+  {
+    check = char(check ^ data.at(i));
+  }
+
+  std::string hex_check;
+
+  hex_check = fmt::format("{0:x}", check);
+
+  return hex_check == fragments[1];
 }
 
 std::string Utils::get_type(std::string sample)
@@ -174,6 +168,15 @@ double Utils::get_speed(std::string speed, std::string units)
   }
 
   return parsedSpeed;
+}
+
+void Utils::clear_screen()
+{
+#ifdef _WIN32
+  std::system("cls");
+#else
+  std::system("clear");
+#endif
 }
 
 #endif // UTILS_H
